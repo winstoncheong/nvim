4f8a1dd61500b026ab0ab0599f5c6f5ad2170bb3
Winston Cheong <cheong.winston1337@gmail.com>
2024-06-02 15:28:39 -0500

    Cleaning up, added firenvim for in-browser nvim.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 
 Started from kickstart.nvim.
 Will clean and adjust from there.
+
+For installation, follow instructions at 
+https://github.com/nvim-lua/kickstart.nvim
--- init.lua
+++ init.lua
@@ -703,31 +703,16 @@ require('lazy').setup(
     { -- Collection of various small independent plugins/modules
       'echasnovski/mini.nvim',
       config = function()
-        -- Better Around/Inside textobjects
-        --
-        -- Examples:
-        --  - va)  - [V]isually select [A]round [)]paren
-        --  - yinq - [Y]ank [I]nside [N]ext [']quote
-        --  - ci'  - [C]hange [I]nside [']quote
         require('mini.ai').setup { n_lines = 500 }
 
-        -- Add/delete/replace surroundings (brackets, quotes, etc.)
-        --
-        -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-        -- - sd'   - [S]urround [D]elete [']quotes
-        -- - sr)'  - [S]urround [R]eplace [)] [']
         require('mini.surround').setup()
 
-        -- Simple and easy statusline.
-        --  You could remove this setup call if you don't like it,
-        --  and try some other statusline plugin
         local statusline = require 'mini.statusline'
-        -- set use_icons to true if you have a Nerd Font
+
+        -- Set use_icons to true if you have a Nerd Font
         statusline.setup { use_icons = vim.g.have_nerd_font }
 
-        -- You can configure sections in the statusline by overriding their
-        -- default behavior. For example, here we set the section for
-        -- cursor location to LINE:COLUMN
+        -- Set the section for cursor location to LINE:COLUMN
         ---@diagnostic disable-next-line: duplicate-set-field
         statusline.section_location = function()
           return '%2l:%-2v'
@@ -840,7 +825,16 @@ require('lazy').setup(
       },
       config = true,
     },
+    {
+      'glacambre/firenvim',
 
+      -- Lazy load firenvim
+      -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+      lazy = not vim.g.started_by_firenvim,
+      build = function()
+        vim.fn['firenvim#install'](0)
+      end,
+    },
     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
     --    This is the easiest way to modularize your config.
     --
