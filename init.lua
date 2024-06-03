8c504fb03e33509437d8c9fc5d2aebac13da851d
Winston Cheong <cheong.winston1337@gmail.com>
2024-06-03 13:13:48 -0500

    Plugins for html

--- init.lua
+++ init.lua
@@ -906,28 +906,17 @@ require('lazy').setup(
         require('window-picker').setup()
       end,
     },
-
     {
       'windwp/nvim-ts-autotag',
-      event = 'VeryLazy',
+      lazy = false,
+      dependencies = 'nvim-treesitter/nvim-treesitter',
       config = function()
-        require('nvim-ts-autotag').setup {
-          opts = {
-            -- Defaults
-            enable_close = true, -- Auto close tags
-            enable_rename = true, -- Auto rename pairs of tags
-            enable_close_on_slash = false, -- Auto close on trailing </
-          },
-          -- Also override individual filetype configs, these take priority.
-          -- Empty by default, useful if one of the "opts" global settings
-          -- doesn't work well in a specific filetype
-          per_filetype = {
-            ['html'] = {
-              enable_close = false,
-            },
-          },
-        }
+        require('nvim-ts-autotag').setup()
       end,
+    },
+    {
+      'mattn/emmet-vim',
+      event = 'VeryLazy',
     },
     {
       'tadmccorkle/markdown.nvim',
