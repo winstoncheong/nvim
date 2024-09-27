4c02ec9c1773b1ffa8ec2de7948ce1b2ce99387c
Winston Cheong <cheong.winston1337@gmail.com>
2024-09-26 20:10:44 -0500

    removed firenvim

--- init.lua
+++ init.lua
@@ -898,16 +898,6 @@ require('lazy').setup(
       config = true,
     },
     {
-      'glacambre/firenvim',
-
-      -- Lazy load firenvim
-      -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-      lazy = not vim.g.started_by_firenvim,
-      build = function()
-        vim.fn['firenvim#install'](0)
-      end,
-    },
-    {
       's1n7ax/nvim-window-picker',
       name = 'window-picker',
       event = 'VeryLazy',
