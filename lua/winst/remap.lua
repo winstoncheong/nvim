71749fa68184b0a72c223d95d1c685b0876c8451
Winston Cheong <cheong.winston1337@gmail.com>
2025-04-20 03:39:33 -0500

    add remap for folding in tex files

--- lua/winst/remap.lua
+++ lua/winst/remap.lua
@@ -4,3 +4,15 @@ vim.keymap.set('n', '<space>s', require('nvim-tree.api').tree.toggle, {
 	silent=true,
 	desc ='toggle nvim-tree',
 })
+
+-- mapping for easier folding in tex files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "tex",
+  callback = function(event)
+    vim.keymap.set("n", "<space><tab>", "za", {  
+      buffer = event.buf,  -- make mapping local to buffer
+      silent=true,
+      desc ='toggle fold',
+    })
+  end,
+})
