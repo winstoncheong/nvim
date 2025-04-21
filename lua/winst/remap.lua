d810098630512da5999c328f7ced747141990c88
Winston Cheong <cheong.winston1337@gmail.com>
2025-04-21 08:13:30 -0500

    Added settings from older version, which was based off of kickstart.nvim

--- lua/winst/remap.lua
+++ lua/winst/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<space>s', require('nvim-tree.api').tree.toggle, {
+vim.keymap.set('n', '<C-\\>', require('nvim-tree.api').tree.toggle, {
 	silent=true,
 	desc ='toggle nvim-tree',
 })
@@ -17,10 +17,81 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "tex",
   callback = function(event)
-    vim.keymap.set("n", "<space><tab>", "za", {
+    vim.keymap.set("n", "<leader>a", "za", {
       buffer = event.buf,  -- make mapping local to buffer
       silent=true,
       desc ='toggle fold',
     })
   end,
 })
+
+-- Easier switch to alternate file
+vim.keymap.set('n', '<leader><tab>', ':e #<CR>', { desc = 'switch to alternate file' })
+
+vim.keymap.set('n', 'n', 'nzz') -- make search result appear vertically centered
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+
+-- Highlight when yanking (copying) text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
+-- Diagnostic keymaps
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+-- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+
+
+
+
+-- Keybinds to make split navigation easier.
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+
+-- Resize with arrows
+vim.keymap.set('n', '<C-Up>', ':resize +2<CR>', {})
+vim.keymap.set('n', '<C-Down>', ':resize -2<CR>', {})
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', {})
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', {})
+
+-- Navigate buffers
+vim.keymap.set('n', '<S-l>', ':bnext<CR>', {})
+vim.keymap.set('n', '<S-h>', ':bprevious<CR>', {})
+
+
+
+-- Move text up and down
+-- vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', {})
+-- vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', {})
+-- in visual mode
+-- vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", {})
+-- vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", {})
+-- vim.keymap.set('v', 'p', '"_dP', {})
+-- in visual block mode
+-- vim.keymap.set('x', 'J', ":m '>+1<CR>gv=gv", {})
+-- vim.keymap.set('x', 'K', ":m '<-2<CR>gv=gv", {})
+-- vim.keymap.set('x', '<A-j>', ":m '>+1<CR>gv=gv", {})
+-- vim.keymap.set('x', '<A-k>', ":m '<-2<CR>gv=gv", {})
+
+
+
+-- Stay in visual mode when indent text
+vim.keymap.set('v', '<', '<gv^', {})
+vim.keymap.set('v', '>', '>gv^', {})
+
+-- Easier switch to alternate file
+vim.keymap.set('n', '<leader><tab>', ':e #<CR>', { desc = 'switch to alternate file' })
--- lua/winst/set.lua
+++ lua/winst/set.lua
@@ -1,17 +1,21 @@
 
 vim.cmd.colorscheme("tokyonight")
 
-vim.opt.clipboard = 'unnamedplus' -- use system keyboard for yank
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
 
 vim.opt.nu = true                 -- set line numbers -- set line numbers
 vim.opt.relativenumber = true     -- use relative line numbers
 
 -- set tab size to 2 spaces
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 
 vim.opt.wrap = true
 vim.opt.linebreak = true
@@ -22,3 +26,22 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 vim.opt.termguicolors = true
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Wrap long lines better
+vim.o.linebreak = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
