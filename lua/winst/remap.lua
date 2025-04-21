9ccd7516eaa134b8966d72e3ccb486f88730f369
Winston Cheong <cheong.winston1337@gmail.com>
2025-04-21 03:44:57 -0500

    add telescope keybinds

--- lua/winst/remap.lua
+++ lua/winst/remap.lua
@@ -5,6 +5,13 @@ vim.keymap.set('n', '<space>s', require('nvim-tree.api').tree.toggle, {
 	desc ='toggle nvim-tree',
 })
 
+-- some keymaps for telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
 -- mapping for easier folding in tex files
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "tex",
