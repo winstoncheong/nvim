be70aba632b10c907a45cf7f0ce0fa2daa5f7294
Winston Cheong <cheong.winston1337@gmail.com>
2025-04-21 04:53:18 -0500

    add keymap for oldfiles, reorder lines for importance

--- lua/winst/remap.lua
+++ lua/winst/remap.lua
@@ -7,9 +7,10 @@ vim.keymap.set('n', '<space>s', require('nvim-tree.api').tree.toggle, {
 
 -- some keymaps for telescope
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Telescope previously open files' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' }) -- in CWD
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- mapping for easier folding in tex files
